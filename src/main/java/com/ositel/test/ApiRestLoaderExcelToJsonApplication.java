66a05acb78c70f3c9ead611fa47d2c2156a83584
Mor Talla Ndiaye <comptetalla@gmail.com>
2018-09-22 09:35:02 +0000

    Initialisation of the rest api

---
+++ src/main/java/com/ositel/test/ApiRestLoaderExcelToJsonApplication.java
@@ -0,0 +1,12 @@
+package com.ositel.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestLoaderExcelToJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestLoaderExcelToJsonApplication.class, args);
+	}
+}
